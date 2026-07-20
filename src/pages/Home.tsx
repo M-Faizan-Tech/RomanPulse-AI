75eaa185b68f6a0eebb87e6bde909dae1fcf5943
M-Faizan-Tech <itsfaiz325@gmail.com>
2026-07-20 17:39:43 +0500

    feat: setup application routing structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      RomanPulse AI Landing Page
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      Login Page
+    </div>
+  );
+}
+
+export default Login;
