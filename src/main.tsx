8fd60e725f8da8aa328d55e3c68b3ca0d13cc3c9
M-Faizan-Tech <itsfaiz325@gmail.com>
2026-07-20 17:46:57 +0500

    feat: create premium design system foundation

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
 import "./index.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
